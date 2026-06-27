7a66ba378b4c3e863c6f8f60916ebc8c21040d62
Leonardo Silveira <sombriks@gmail.com>
2026-06-27 12:59:56 -0300

    some tests

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,15 @@
+const std = @import("std");
 /// checks if a type is even
 pub fn izEven(a: anytype) bool {
-    return a % 2;
+    return a % 2 == 0;
+}
+
+test "shoud be true" {
+    const even = izEven(8);
+    try std.testing.expect(even);
+}
+
+test "shoud be false" {
+    const odd = !izEven(7);
+    try std.testing.expect(odd);
 }
